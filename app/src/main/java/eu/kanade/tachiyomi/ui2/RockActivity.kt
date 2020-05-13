58a7a4b41975db22b46828494220e89fd562c0e2
Chris Weber <chris.weber@bolinda.com>
2020-05-13 23:16:12 +0200

    [F] initial commit for the new ui, creating base activity

---
+++ app/src/main/java/eu/kanade/tachiyomi/ui2/RockActivity.kt
@@ -0,0 +1,13 @@
+package eu.kanade.tachiyomi.ui2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import eu.kanade.tachiyomi.R
+
+class RockActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rock)
+    }
+}
